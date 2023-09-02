a12373d4339659af524029f7d52620efd69901ad
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-09-02 18:05:14 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,17 @@ public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		System.out.println("Hello World");
 		File submissionFile = new File("Lab1.sql");
 		Scanner sc = new Scanner(submissionFile);
 		while (sc.hasNextLine())
 		{
-		    System.out.println(sc.nextLine());
+		    //System.out.println(sc.nextLine());
+		    String line = sc.nextLine();
+		    //If the line is a comment, it is useless to us.
+		    if (line.startsWith("--"))
+		    {
+		        System.out.println(line);
+		    }
 		}
 		sc.close();
 	}
