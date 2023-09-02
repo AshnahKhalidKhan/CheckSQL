17c18bae84adf8a52cad6413c2e154c216bd3b6b
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-09-02 22:11:38 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,25 +5,76 @@ public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		File submissionFile = new File("Lab1.sql");
-		Scanner sc = new Scanner(submissionFile);
+	    int TotalQuestions = 27;
+	    
+	    String[] Answers = new String[TotalQuestions];
+	    String[] Submission = new String[TotalQuestions];
+	    
+	    File AnswerFile = new File("Answer.sql");
+		Scanner sc = new Scanner(AnswerFile);
+		int n = 0;
 		while (sc.hasNextLine())
 		{
-		    //System.out.println(sc.nextLine());
+		    String line = sc.nextLine();
+		    line = line.toLowerCase();
+		    Answers[n] = line;
+		    n++;
+		}
+		sc.close();
+	    
+		File StudentSubmissionFile = new File("Lab1.sql");
+		sc = new Scanner(StudentSubmissionFile);
+		n = 0;
+		while (sc.hasNextLine())
+		{
 		    String line = sc.nextLine();
 		    //If the line is a comment, it is useless to us.
-		    if (line.startsWith("--") == false)
+		    if (line.startsWith("--") == false && n < TotalQuestions)
 		    {
 		        //Make string lowercase
 		        line = line.toLowerCase();
 		        //Save in array
-		        //Compare arrays
-		        //Count correct ones
-		        //Count incorrect ones and mark them
-		        //Generate final mark
-		        System.out.println(line);
+		        Submission[n] = line;
+		        n++;
 		    }
 		}
 		sc.close();
+		
+		int CorrectAnswers = 0;
+		String IncorrectAnswers = "";
+		for (int i = 0; i < TotalQuestions; i++)
+		{
+		    if (Answers[i] == Submission[i])
+		    {
+		        CorrectAnswers++;
+		    }
+		    else
+		    {
+		        System.out.println((i + 1) + ".\nAnswer: " + Answers[i] + "\nSubmission: " + Submission[i] + "\n");
+		        if (IncorrectAnswers == "")
+		        {
+		            IncorrectAnswers = IncorrectAnswers + (i + 1);
+		        }
+		        else
+		        {
+		            IncorrectAnswers = IncorrectAnswers + ", " + (i + 1);
+		        }
+		    }
+		}
+		
+// 		System.out.println("PRINTING ANSWERS HERE");
+// 		for (int i = 0; i < TotalQuestions; i++)
+// 		{
+// 		    System.out.println(Answers[i]);
+// 		}
+	
+// 		System.out.println("PRINTING SUBMISSION HERE");	
+// 		for (int i = 0; i < TotalQuestions; i++)
+// 		{
+// 		    System.out.println(Submission[i]);
+// 		}
+		
+		double FinalMarks = ((double) CorrectAnswers / (double) TotalQuestions) * 6.0;
+	    System.out.println("FINAL GRADE:\nCorrect Answers = " + CorrectAnswers + "\nIncorrect Answers = " + IncorrectAnswers + "\nScore = " + FinalMarks);
 	}
 }
