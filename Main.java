327446f08b8687b7ad103247f5aa7e87558d95fc
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-09-02 17:53:30 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,12 @@ public class Main
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		System.out.println("Hello World");
-		File submissionFile = new File("Lab1.txt");
+		File submissionFile = new File("Lab1.sql");
 		Scanner sc = new Scanner(submissionFile);
 		while (sc.hasNextLine())
 		{
 		    System.out.println(sc.nextLine());
 		}
+		sc.close();
 	}
 }
