098ab5fc7be79cb876e2346d2e6f91aff6647c56
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-09-03 17:28:15 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
 
 public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		Screen frame = new Screen();
+
 	    int TotalQuestions = 27;
 	    
 	    String[] Answers = new String[TotalQuestions];
