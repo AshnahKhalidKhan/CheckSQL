55acc2be0ad61abf598c8a9750df5f9421d40dbe
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-06 23:55:17 +0500

    default values

--- lazychex/src/App.js
+++ lazychex/src/App.js
@@ -7,8 +7,8 @@ function App() {
         <h1>LazyCheX or CheckSQL</h1>
       </div>
       <div style = {{width: '100%', textAlign: 'center', margin: 'auto'}}>
-        <input type = 'file' placeholder = 'Answer file goes here' style = {{textAlign: 'center', margin: 'auto'}}></input>
-        <input type = 'file' placeholder = 'Submission file goes here' style = {{textAlign: 'center', margin: 'auto'}}></input>
+        <input type = 'file' defaultValue = 'Answer file goes here' style = {{textAlign: 'center', margin: 'auto'}}></input>
+        <input type = 'file' defaultValue = 'Submission file goes here' style = {{textAlign: 'center', margin: 'auto'}}></input>
       </div>
       <table style = {{width: '100%'}}>
         <thead>
