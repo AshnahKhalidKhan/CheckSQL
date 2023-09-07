167e37fd4c4ad4f7dd2f92d9f1a6f31d7ce6697c
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-07 12:16:31 +0500

    A lot of QueryBox CSSS

--- lazychex/src/Components/QueryBox/QueryBox.js
+++ lazychex/src/Components/QueryBox/QueryBox.js
@@ -4,8 +4,8 @@ import { CheckBox } from '@mui/icons-material';
 function QueryBox() {
     return (
         <div className = 'QueryBox'>
-            <CheckBox/>
-            <h1>Something something</h1>
+            <h1 style = {{textAlign: 'center', margin: 'auto'}}>Something something</h1>
+            <h1 style = {{textAlign: 'center', margin: 'auto'}}>Something something</h1>
         </div>
     );
 }
