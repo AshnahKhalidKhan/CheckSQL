9a2b72e225f363b9ab746e97aba82fa146ef4d7b
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-07 20:37:50 +0500

    We cannot center anything, friend

--- lazychex/src/Components/QueryBox/QueryBox.js
+++ lazychex/src/Components/QueryBox/QueryBox.js
@@ -1,34 +1,10 @@
 import './QueryBox.css';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-
-const VisuallyHiddenInput = styled('input')`
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  white-space: nowrap;
-  width: 1px;
-`;
 
 function QueryBox() {
     return (
         <div className = 'QueryBox'>
-            {/* <h1 style = {{}}>Something something</h1> */}
-            {/* <input id = 'AnswerFileInput' type = 'file' placeholder = 'Answer file goes here' style = {{}}></input> */}
-            <Button
-  component="label"
-  variant="contained"
-  startIcon={<CloudUploadIcon />}
-  href="#file-upload"
->
-  Upload a file
-  <VisuallyHiddenInput type="file" />
-</Button>
+            <h1>Something something</h1>
+            <input id = 'AnswerFileInput' type = 'file'></input>
             <h1 >Something something</h1>
         </div>
     );
