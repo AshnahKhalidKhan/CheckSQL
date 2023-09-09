925962ff47c187f950e5827922fc12737c051856
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-09 22:16:34 +0500

    Something big is happening brother...

--- lazychex/src/Components/FileInput/FileInput.js
+++ lazychex/src/Components/FileInput/FileInput.js
@@ -2,14 +2,19 @@
 import React, { useState } from 'react';
 
 function FileInput({ onFileSelect }) {
-  const handleFileChange = (event) => {
+  const handleFileChange = (event) =>
+  {
     const file = event.target.files[0];
+    // ^event.target refers to the input element, and .files is an array containing the selected files. We assume there's only one selected file ([0]).
 
-    if (file) {
+    if (file != null && file != undefined)
+    {
       const reader = new FileReader();
-
-      reader.onload = (e) => {
+      // We set up an onload event handler for the reader. When the file is successfully loaded, this handler is called.
+      reader.onload = (e) =>
+      {
         const fileContent = e.target.result;
+        // ^Inside the onload handler, we access the file's content using e.target.result. This content is in the form of a string.
         onFileSelect(fileContent);
       };
 
