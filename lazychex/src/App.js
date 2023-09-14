8eedba01e91287a58434804527c5f88d07a7bc5f
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-09-14 20:02:20 +0500

    We're gonna be debugging like craaaaazyyyy here

--- lazychex/src/App.js
+++ lazychex/src/App.js
@@ -1,145 +1,276 @@
+// import "./App.css";
+// import FileInput from "./Components/FileInput/FileInput";
+// import QueryBox from "./Components/QueryBox/QueryBox";
+// import React, {useState, useEffect} from "react";
+
+// function App() {
+//   /*
+//     1. Create separate state variables constants.
+//     2. Create function to set state variables ya kya????
+//     3. Include the actual thang into the component"s div????
+//   */
+//   const [AnswerFileContent, setAnswerFileContent] = useState([]);
+//   const [SubmissionFileContent, setSubmissionFileContent] = useState([]);
+//   const [CorrectAnswers, setCorrectAnswers] = useState(0);
+
+//   const handleAnswerFileSelect = (line) =>
+//   {
+//     setAnswerFileContent((previousLines) => [...previousLines, line]);
+//   };
+//   const handleSubmissionFileSelect = (line) =>
+//   {
+//     setSubmissionFileContent((previousLines) => [...previousLines, line]);
+//   };
+
+//   // Function to clear the content
+//   const clearAnswerFileInputContent = () =>
+//   {
+//     setAnswerFileContent([]);
+//   };
+
+//   const clearSubmissionFileInputContent = () =>
+//   {
+//     setSubmissionFileContent([]);
+//   };
+
+//   const selectTickOrCross = (AnswerText, SubmissionText) =>
+//   {
+//     if (AnswerText == SubmissionText)
+//     {
+//       return "Tick.png";
+//     }
+//     else
+//       return "Cross.png";
+//   };
+
+//   const toggleImage = (index) => {
+//     const newAnswerFileContent = [...AnswerFileContent];
+//     if (newAnswerFileContent[index] === "Tick.png") {
+//       newAnswerFileContent[index] = "Cross.png";
+//       setCorrectAnswers((count) => count - 1);
+//     } else {
+//       newAnswerFileContent[index] = "Tick.png";
+//       setCorrectAnswers((count) => count + 1);
+//     }
+//     setAnswerFileContent(newAnswerFileContent);
+//   };
+
+//   useEffect(() =>
+//   {
+//     let correctAnswers = 0;
+//     for (let index = 0; index < Math.min(AnswerFileContent.length, SubmissionFileContent.length); index++)
+//     {
+//       if (AnswerFileContent[index] !== "" && SubmissionFileContent[index] !== "" && AnswerFileContent[index] === SubmissionFileContent[index])
+//       {
+//         correctAnswers++;
+//       }
+//     }
+//     setCorrectAnswers(correctAnswers);
+//   }, [AnswerFileContent, SubmissionFileContent]);
+
+//   // Define the number of rows based on the maximum lines between the two files.
+//   const totalQuestions = Math.max(AnswerFileContent.length, SubmissionFileContent.length);
+
+//   return (
+//     <div>
+//       <div className = "Title">
+//         <h1>LazyCheX or CheckSQL</h1>
+//       </div>
+//       <div>
+//         <h1>{CorrectAnswers}</h1>
+//       </div>
+
+
+
+//       <div className = "Section">
+//         <div className = "MakeThemAppearSideBySideInSameLine">
+//           <FileInput id = "AnswerFileInput" onFileSelect = {handleAnswerFileSelect} uniqueKeyPropToDifferentiateInputs = "AnswerFileInput" clearContent = {clearAnswerFileInputContent}/>
+//         </div>
+//         <div className = "MakeThemAppearSideBySideInSameLine">
+//           <FileInput id = "SubmissionFileInput" onFileSelect = {handleSubmissionFileSelect} uniqueKeyPropToDifferentiateInputs = "SubmissionFileInput" clearContent = {clearSubmissionFileInputContent}/>
+//         </div>
+//       </div>
+
+//       {/* THIS IS AN IMPORTANT COMMENT ---> We learned a lot from this, Ash.
+//       {AnswerFileContent.map((line, index) =>
+//         (
+//           <QueryBox id = {`AnswerFileInputDisplay${index}`} fileContent = {line} key = {`AnswerFileInputDisplay${index}`}/>
+//         )
+//       )}
+//       {SubmissionFileContent.map((line, index) =>
+//         (
+//           <QueryBox id = {`SubmissionFileInputDisplay${index}`} fileContent = {line} key = {`SubmissionFileInputDisplay${index}`}/>
+//         )
+//       )} */}
+
+//       <table>
+//         <thead>
+//           <th className = "EqualWidthColumns">
+//             Answer File
+//           </th>
+//           <th className = "EqualWidthColumns">
+//             Submission File
+//           </th>
+//           <th className = "ColumnWithOneFifthWidthOfTable">
+//             Mark
+//           </th>
+//         </thead>
+//         <tbody>
+//           {Array.from({length: totalQuestions}).map((_, index) =>
+//             (
+//               <tr key = {`row${index}`}>
+//                 <td>
+//                   <QueryBox id = {`AnswerFileInputDisplay${index}`} fileContent = {AnswerFileContent[index] || ""} key = {`AnswerFileInputDisplay${index}`}/>
+//                 </td>
+//                 <td>
+//                   <QueryBox id = {`SubmissionFileInputDisplay${index}`} fileContent = {SubmissionFileContent[index] || ""} key = {`SubmissionFileInputDisplay${index}`}/>
+//                 </td>
+//                 <td>
+//                   <img src = {selectTickOrCross(AnswerFileContent[index], SubmissionFileContent[index])} onClick={() => toggleImage(index)}/>
+//                 </td>
+//               </tr>
+//             )
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import FileInput from "./Components/FileInput/FileInput";
 import QueryBox from "./Components/QueryBox/QueryBox";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  /*
-    1. Create separate state variables constants.
-    2. Create function to set state variables ya kya????
-    3. Include the actual thang into the component"s div????
-  */
   const [AnswerFileContent, setAnswerFileContent] = useState([]);
   const [SubmissionFileContent, setSubmissionFileContent] = useState([]);
   const [CorrectAnswers, setCorrectAnswers] = useState(0);
-  const [isTick, setIsTick] = useState(false);
 
-  const handleAnswerFileSelect = (line) =>
-  {
+  const handleAnswerFileSelect = (line) => {
     setAnswerFileContent((previousLines) => [...previousLines, line]);
   };
-  const handleSubmissionFileSelect = (line) =>
-  {
+
+  const handleSubmissionFileSelect = (line) => {
     setSubmissionFileContent((previousLines) => [...previousLines, line]);
   };
 
-  // Function to clear the content
-  const clearAnswerFileInputContent = () =>
-  {
+  const clearAnswerFileInputContent = () => {
     setAnswerFileContent([]);
   };
 
-  const clearSubmissionFileInputContent = () =>
-  {
+  const clearSubmissionFileInputContent = () => {
     setSubmissionFileContent([]);
   };
 
-  const selectTickOrCross = (AnswerText, SubmissionText) =>
-  {
-    if (AnswerText == SubmissionText)
-    {
+  const selectTickOrCross = (AnswerText, SubmissionText) => {
+    if (AnswerText === SubmissionText) {
       return "Tick.png";
-    }
-    else
+    } else {
       return "Cross.png";
+    }
   };
 
-  useEffect(() =>
-  {
+  const toggleImage = (index) => {
+    const newAnswerFileContent = [...AnswerFileContent];
+    if (newAnswerFileContent[index] === "Tick.png") {
+      newAnswerFileContent[index] = "Cross.png";
+      setCorrectAnswers((count) => count - 1);
+    } else {
+      newAnswerFileContent[index] = "Tick.png";
+      setCorrectAnswers((count) => count + 1);
+    }
+    setAnswerFileContent(newAnswerFileContent);
+  };
+
+  useEffect(() => {
     let correctAnswers = 0;
-    for (let index = 0; index < Math.min(AnswerFileContent.length, SubmissionFileContent.length); index++)
-    {
-      if (AnswerFileContent[index] !== "" && SubmissionFileContent[index] !== "" && AnswerFileContent[index] === SubmissionFileContent[index])
-      {
+    for (
+      let index = 0;
+      index <
+      Math.min(AnswerFileContent.length, SubmissionFileContent.length);
+      index++
+    ) {
+      if (
+        AnswerFileContent[index] !== "" &&
+        SubmissionFileContent[index] !== "" &&
+        AnswerFileContent[index] === SubmissionFileContent[index]
+      ) {
         correctAnswers++;
       }
     }
     setCorrectAnswers(correctAnswers);
   }, [AnswerFileContent, SubmissionFileContent]);
 
-  // Function to toggle the image source
-  const toggleImage = () => {
-    if (isTick) {
-      setIsTick(false);
-    } else {
-      setIsTick(true);
-    }
-  };
-
-  let imgSrc;
-  if (isTick) {
-    imgSrc = "Tick.png";
-  } else {
-    imgSrc = "Cross.png";
-  }
-
-  // Define the number of rows based on the maximum lines between the two files.
-  const totalQuestions = Math.max(AnswerFileContent.length, SubmissionFileContent.length);
+  const totalQuestions = Math.max(
+    AnswerFileContent.length,
+    SubmissionFileContent.length
+  );
 
   return (
     <div>
-      <div className = "Title">
+      <div className="Title">
         <h1>LazyCheX or CheckSQL</h1>
       </div>
       <div>
         <h1>{CorrectAnswers}</h1>
       </div>
-      <div>
-        <h1>Toggle Image</h1>
-        <img src={imgSrc} alt="Tick or Cross" onClick={toggleImage} />
-      </div>
 
-
-
-      <div className = "Section">
-        <div className = "MakeThemAppearSideBySideInSameLine">
-          <FileInput id = "AnswerFileInput" onFileSelect = {handleAnswerFileSelect} uniqueKeyPropToDifferentiateInputs = "AnswerFileInput" clearContent = {clearAnswerFileInputContent}/>
+      <div className="Section">
+        <div className="MakeThemAppearSideBySideInSameLine">
+          <FileInput
+            id="AnswerFileInput"
+            onFileSelect={handleAnswerFileSelect}
+            uniqueKeyPropToDifferentiateInputs="AnswerFileInput"
+            clearContent={clearAnswerFileInputContent}
+          />
         </div>
-        <div className = "MakeThemAppearSideBySideInSameLine">
-          <FileInput id = "SubmissionFileInput" onFileSelect = {handleSubmissionFileSelect} uniqueKeyPropToDifferentiateInputs = "SubmissionFileInput" clearContent = {clearSubmissionFileInputContent}/>
+        <div className="MakeThemAppearSideBySideInSameLine">
+          <FileInput
+            id="SubmissionFileInput"
+            onFileSelect={handleSubmissionFileSelect}
+            uniqueKeyPropToDifferentiateInputs="SubmissionFileInput"
+            clearContent={clearSubmissionFileInputContent}
+          />
         </div>
       </div>
-
-      {/* THIS IS AN IMPORTANT COMMENT ---> We learned a lot from this, Ash.
-      {AnswerFileContent.map((line, index) =>
-        (
-          <QueryBox id = {`AnswerFileInputDisplay${index}`} fileContent = {line} key = {`AnswerFileInputDisplay${index}`}/>
-        )
-      )}
-      {SubmissionFileContent.map((line, index) =>
-        (
-          <QueryBox id = {`SubmissionFileInputDisplay${index}`} fileContent = {line} key = {`SubmissionFileInputDisplay${index}`}/>
-        )
-      )} */}
 
       <table>
         <thead>
-          <th className = "EqualWidthColumns">
-            Answer File
-          </th>
-          <th className = "EqualWidthColumns">
-            Submission File
-          </th>
-          <th className = "ColumnWithOneFifthWidthOfTable">
-            Mark
-          </th>
+          <tr>
+            <th className="EqualWidthColumns">Answer File</th>
+            <th className="EqualWidthColumns">Submission File</th>
+            <th className="ColumnWithOneFifthWidthOfTable">Mark</th>
+          </tr>
         </thead>
         <tbody>
-          {Array.from({length: totalQuestions}).map((_, index) =>
-            (
-              <tr key = {`row${index}`}>
-                <td>
-                  <QueryBox id = {`AnswerFileInputDisplay${index}`} fileContent = {AnswerFileContent[index] || ""} key = {`AnswerFileInputDisplay${index}`}/>
-                </td>
-                <td>
-                  <QueryBox id = {`SubmissionFileInputDisplay${index}`} fileContent = {SubmissionFileContent[index] || ""} key = {`SubmissionFileInputDisplay${index}`}/>
-                </td>
-                <td>
-                  <img src = {selectTickOrCross(AnswerFileContent[index], SubmissionFileContent[index])}/>
-                </td>
-              </tr>
-            )
-          )}
+          {Array.from({ length: totalQuestions }).map((_, index) => (
+            <tr key={`row${index}`}>
+              <td>
+                <QueryBox
+                  id={`AnswerFileInputDisplay${index}`}
+                  fileContent={AnswerFileContent[index] || ""}
+                  key={`AnswerFileInputDisplay${index}`}
+                />
+              </td>
+              <td>
+                <QueryBox
+                  id={`SubmissionFileInputDisplay${index}`}
+                  fileContent={SubmissionFileContent[index] || ""}
+                  key={`SubmissionFileInputDisplay${index}`}
+                />
+              </td>
+              <td>
+                <img
+                  src={AnswerFileContent[index]}
+                  onClick={() => toggleImage(index)}
+                  style={{ cursor: "pointer" }}
+                />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
